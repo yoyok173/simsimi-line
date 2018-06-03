ebf83040d64262541e4bac2693b11030963ccec3
yoyok173 <saied.five@gmail.com>
2018-06-03 12:12:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = '8ea8d7d3-0713-4fe4-bd49-0d0a2accc5b3'; //API SimSimi
+$key = 'e4c3734fa7d302f38d4d4ded8c922cde'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
